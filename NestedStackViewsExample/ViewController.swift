9430dbd1f234a0e5c2dd4ee19db1a951ddbe97e3
Peter Thomas Horn <peter.horn@open-xchange.com>
2020-01-28 10:27:36 +0100

    Initial Commit

---
+++ NestedStackViewsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NestedStackViewsExample
+//
+//  Created by Peter Thomas Horn on 28.01.20.
+//  Copyright © 2020 Peter Thomas Horn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
